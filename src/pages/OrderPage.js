b9580faf746f1ed33753a0fd1f05c180fb263184
muhammedZak <zakariya.zaku018@gmail.com>
2023-11-28 15:07:26 +0530

    modified ui

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -92,91 +92,134 @@ const OrderPage = () => {
     <p>error fething data..{error.toString()}</p>
   ) : (
     <>
-      <div className=" bg-gradient-to-r from-blue-100 via-blue-200 to-blue-300  min-h-screen">
-        <div className="container mx-auto flex  py-12">
-          <div className="bg-white w-full  rounded-lg shadow-lg p-8 text-gray-800">
-            <h1 className="text-4xl font-semibold mb-8 text-center">
-              Order Details
-            </h1>
-
-            <div className="bg-gray-100 p-6 rounded-lg mb-6">
-              <h2 className="text-3xl font-semibold mb-4">Order Summary</h2>
-
-              {/* Product Card */}
-
-              {order.orderItems &&
-                order.orderItems?.map((item) => (
-                  <div
-                    key={item._id}
-                    className="border-b border-gray-300 pb-2 mb-2 grid grid-cols-2 items-center gap-4"
-                  >
-                    <div className="flex items-center space-x-4">
-                      <img
-                        src={item?.images[0].path}
-                        alt={item?.name}
-                        className="h-16 w-16 object-cover rounded-lg"
-                      />
-                      <div>
-                        <p className="text-xl font-semibold">{item?.name}</p>
-                        <p className="text-gray-600">Quantity: {item?.qty}</p>
-                      </div>
+      <div className="px-4 mb-6">
+        <div className="p-2 my-2 border">
+          <h1 className="text-lg lg:text-2xl font-medium tracking-wide">
+            Order details
+          </h1>
+        </div>
+        <div className="md:flex justify-between gap-3">
+          <div className="md:w-2/3">
+            <div className="p-2 my-1 bg-gray-100">
+              <h5 className="font-medium">Cart items</h5>
+            </div>
+            <div className=" border">
+              <div className="p-2 my-1 flex justify-between">
+                {order.orderItems?.map((item) => (
+                  <>
+                    <img
+                      className="w-40 lg:w-36 h-48 lg:h-44 object-contain"
+                      src={item.images[0].path}
+                      alt="Image"
+                    />
+                    <div>
+                      <p className=" text-xl tracking-wider font-semibold font-serif text-ellipsis overflow-hidden text-neutral-600">
+                        {item.name}
+                      </p>
+                      <p className=" text-lg tracking-wider font-serif text-ellipsis overflow-hidden text-neutral-600">
+                        {item.description}
+                      </p>
+                      <p className="text-lg font-serif font-semibold leading-10 tracking-wider">
+                        Quantity: {item.qty}
+                      </p>
+                      <p className="text-lg font-serif font-semibold leading-10 tracking-wider">
+                        Price: ₹{item.price}
+                      </p>
                     </div>
-                    <p className="text-xl font-semibold">
-                      ${(item?.qty * item?.price).toFixed(2)}
-                    </p>
-                  </div>
+                  </>
                 ))}
+              </div>
             </div>
-            {/* Shipping Details */}
-            <div className="bg-gray-100 p-6 rounded-lg mb-6">
-              <h2 className="text-3xl font-semibold mb-4">Shipping Details</h2>
-              <p className="text-gray-600">
-                Name: {order.shippingAddress?.contact.name}
-              </p>
-              <p className="text-gray-600">
-                Address: {order.shippingAddress?.addressDetails.address},{' '}
-                {order.shippingAddress?.addressDetails.town},{' '}
-                {order.shippingAddress?.addressDetails.pin},{' '}
-                {order.shippingAddress?.addressDetails.country}
-              </p>
-              <p className="text-gray-600">
-                Paid: {order?.isPaid ? 'Paid' : 'Not paid'}
-              </p>
-              <p className="text-gray-600">Name: {order.user?.name}</p>
+
+            <div className="p-2 my-1 bg-gray-100">
+              <h5 className="font-medium">Shipping address</h5>
             </div>
-            {/* Order Total */}
-            <div className="bg-gray-100 p-6 rounded-lg mb-6">
-              <h2 className="text-3xl font-semibold mb-4">Order Total</h2>
-              <div className="flex justify-between items-center">
-                <p className="text-gray-600">Subtotal:</p>
-                <p className="text-xl font-semibold">{order?.itemsPrice}</p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-gray-600">Shipping:</p>
-                <p className="text-xl font-semibold">{order?.shippingPrice}</p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-gray-600">Tax:</p>
-                <p className="text-xl font-semibold">{order?.taxPrice}</p>
-              </div>
-              <div className="flex justify-between items-center border-t border-gray-300 pt-4">
-                <p className="text-2xl font-semibold">Total:</p>
-                <p className="text-4xl font-semibold text-indigo-600">
-                  {order?.totalPrice}
+            <div className=" border">
+              <div className="p-2 my-1">
+                <h5 className=" text-xl tracking-wider font-semibold font-serif text-ellipsis overflow-hidden text-neutral-600">
+                  {order.shippingAddress.contact.name}
+                </h5>
+                <p className=" text-xl tracking-wider font-semibold  text-ellipsis overflow-hidden text-neutral-600">
+                  {order.shippingAddress.contact.mobile}
+                </p>
+                <p className=" text-lg tracking-wider font-serif text-ellipsis overflow-hidden text-neutral-600">
+                  {order.shippingAddress.addressDetails.address}
+                </p>
+                <p className=" text-lg tracking-wider font-serif text-ellipsis overflow-hidden text-neutral-600">
+                  {order.shippingAddress.addressDetails.town},
+                  {order.shippingAddress.addressDetails.district}
+                </p>
+                <p className=" text-lg tracking-wider font-serif text-ellipsis overflow-hidden text-neutral-600">
+                  {order.shippingAddress.addressDetails.state},
+                  {order.shippingAddress.addressDetails.country}
                 </p>
               </div>
             </div>
-            {isPending ? (
-              <Skeleton times={1} className="h-10 w-full" />
-            ) : !order.isPaid ? (
-              <PayPalButtons
-                createOrder={createOrder}
-                onApprove={onApprove}
-                onError={onError}
-              ></PayPalButtons>
-            ) : (
-              ''
-            )}
+            <div className="p-2 my-1 bg-gray-100">
+              <h5 className="font-medium">Payment method</h5>
+            </div>
+            <div className=" border">
+              <div className="p-2 my-1">
+                <p className=" text-xl tracking-wider font-semibold font-serif text-ellipsis overflow-hidden text-neutral-600">
+                  {order.paymentMethod}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/3 my-1">
+            <div className="p-3 border">
+              <h2 className="text-2xl lg:text-xl font-semibold tracking-wider">
+                Order Summary
+              </h2>
+              <div className=" mt-3 flex justify-between">
+                <h4 className="text-xl lg:text-lg font-medium text-orange-950">
+                  Bag Total
+                </h4>
+                <h4 className="font-serif text-xl lg:text-lg font-medium text-orange-950">
+                  ₹{order.itemsPrice}
+                </h4>
+              </div>
+              <hr className="mt-3" />
+              <div className="mt-3 flex justify-between">
+                <h4 className="text-xl lg:text-lg font-medium text-orange-950">
+                  Tax
+                </h4>
+                <h4 className="font-serif text-xl lg:text-lg font-medium text-orange-950">
+                  ₹{order.taxPrice}
+                </h4>
+              </div>
+              <hr className="mt-3" />
+              <div className="mt-3 flex justify-between">
+                <h4 className="text-xl lg:text-lg font-medium text-orange-950">
+                  Delivery
+                </h4>
+                <h4 className="font-serif text-xl lg:text-lg font-medium text-orange-950">
+                  ₹{order.shippingPrice}
+                </h4>
+              </div>
+              <hr className="mt-3" />
+              <div className="mt-3 flex justify-between">
+                <h4 className="text-xl lg:text-lg font-bold text-orange-950">
+                  Total Amount
+                </h4>
+                <h4 className="font-serif text-xl  lg:text-lg font-bold text-orange-950">
+                  ₹{order.totalPrice}
+                </h4>
+              </div>
+              <div>
+                {isPending ? (
+                  <Skeleton times={1} className="h-10 w-full" />
+                ) : !order.isPaid && order.paymentMethod === 'payPal' ? (
+                  <PayPalButtons
+                    createOrder={createOrder}
+                    onApprove={onApprove}
+                    onError={onError}
+                  ></PayPalButtons>
+                ) : (
+                  ''
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
